3cf703828a35182f85f8a8553b0cdf4bfc59fbd8
adelinaxax <adelina1911@gmail.com>
2025-05-21 01:01:05 +0300

    before react

---
+++ src/components/join-the-cause/join-the-cause.ts
@@ -0,0 +1,1 @@
+import './join-the-cause.css';
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,17 @@ const mainButton = document.querySelector<HTMLButtonElement>('.hero-button');
 if (mainButton) {
     initMainBanner(mainButton);
 }
+
+document.querySelectorAll('.footer-title').forEach(button => {
+  button.addEventListener('click', () => {
+    const column = button.closest('.footer-column');
+    column?.classList.toggle('open');
+    
+    // Анимация плюсика
+    const toggle = button.querySelector('.footer-toggle');
+    if (toggle) {
+      toggle.textContent = column?.classList.contains('open') ? '−' : '+';
+    }
+  });
+});
+
