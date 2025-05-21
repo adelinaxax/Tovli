2756d5fbbeef922f6c238c6e47b39c39440be1c1
adelinaxax <adelina1911@gmail.com>
2025-05-21 17:29:06 +0300

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,52 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.tsx',
-
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/',
-        assetModuleFilename: 'assets/[name][ext]'
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.jsx', '.tsx']
-    },
-
-    devServer: {
-        historyApiFallback: true,
-        hot: true,
-        static: {
-            directory: path.join(__dirname, 'dist'),
+  entry: './src/index.tsx',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    clean: true,
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.png|.svg/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [require.resolve('react-refresh/babel')],
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
         },
-        port: 3000,
-        open: true
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {
-                        transpileOnly: true
-                    }
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-              },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/[name][ext]'
-                }
-            },
-        ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            inject: true
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/assets'),
-                    to: path.resolve(__dirname, 'dist/assets'),
-                },
-            ],
-        }),
+      },
+      {
+        test: /\.css$/i,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new ReactRefreshWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
 };
